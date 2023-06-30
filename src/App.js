2e2df1468f8e35771f84b39550ee397bba93e4f3
khawarizmi  azis septian <khawarizmi.azis.ft21@mail.umy.ac.id>
2023-06-30 19:33:53 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+    <h1>BURGER</h1>
+    <p>Burger adalah sejenis roti berbentuk bundar yang diiris dua, dan di tengahnya diisi dengan patty yang biasanya terbuat dari daging, kemudian sayur-sayuran berupa selada, tomat dan bawang bombai. Sebagai sausnya, burger diberi berbagai jenis saus seperti mayones, saus tomat dan sambal, serta mustard. Beberapa varian burger juga dilengkapi dengan keju dan asinan.</p>
+    <img src="https://tse4.mm.bing.net/th?id=OIP.pj9gkdfxDbpqwfcPtgwHuAHaFj&pid=Api&P=0&h=180" alt="Gambar" width="300" height="200"/>
+  </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
